ef3e67cb5eb3fe9616f9561fe17db89aaa8a5f29
Freddy Reyes <reyesolis.f10@gmail.com>
2022-11-04 21:04:47 -0600

    Creacion de formularios de Estudiante

---
+++ PlataformaDKUCorp/Interfaz Alumno/MenuAlumno.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PlataformaDKUCorp.Interfaz_Alumno
+{
+    partial class MenuAlumno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnCerrarSesion = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(172, 164);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 85);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Asignaturas disponibles:";
+            // 
+            // BtnCerrarSesion
+            // 
+            this.BtnCerrarSesion.Location = new System.Drawing.Point(524, 349);
+            this.BtnCerrarSesion.Name = "BtnCerrarSesion";
+            this.BtnCerrarSesion.Size = new System.Drawing.Size(133, 29);
+            this.BtnCerrarSesion.TabIndex = 11;
+            this.BtnCerrarSesion.Text = "Cerrar Sesion";
+            this.BtnCerrarSesion.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ingrese a las asignaturas disponibles";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(230, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 33);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Menu Principal";
+            // 
+            // MenuAlumno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 417);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BtnCerrarSesion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MenuAlumno";
+            this.Text = "MenuAlumno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtnCerrarSesion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PlataformaDKUCorp/Interfaz Alumno/asigAlumno.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PlataformaDKUCorp.Interfaz_Alumno
+{
+    partial class asigAlumno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.NombreClase = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(581, 369);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 45);
+            this.btnRegresar.TabIndex = 10;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(86, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(422, 93);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cuestionarios Disponibles";
+            // 
+            // NombreClase
+            // 
+            this.NombreClase.AutoSize = true;
+            this.NombreClase.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NombreClase.Location = new System.Drawing.Point(54, 9);
+            this.NombreClase.Name = "NombreClase";
+            this.NombreClase.Size = new System.Drawing.Size(278, 33);
+            this.NombreClase.TabIndex = 7;
+            this.NombreClase.Text = "<placeholderNom>";
+            // 
+            // asigAlumno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 423);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.NombreClase);
+            this.Name = "asigAlumno";
+            this.Text = "asigAlumno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label NombreClase;
+    }
+}
